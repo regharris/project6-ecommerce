b58b74adc8f9be408d03cccf9276d4489830530c
Reginald Harris <reginaldharris@Administrators-MacBook-Pro.local>
2019-12-02 15:52:54 -0500

    fixes service worker

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import 'milligram'
-import './styles.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "milligram";
+import "./styles.css";
+import * as serviceWorker from "./serviceWorker";
 
-import App from './App'
+import App from "./App";
 
 ReactDOM.render(
-	<Router><App /></Router>,
-	document.getElementById('root')
-)
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
+
+//makes the app work offline and load data faster
+
+serviceWorker.register();
