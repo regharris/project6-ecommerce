912e5d677931c1887b8f1b596b770f86f1c643a3
Reginald Harris <reginaldharris@Administrators-MacBook-Pro.local>
2019-12-03 11:56:29 -0500

    fixes https

--- server.js
+++ server.js
@@ -32,18 +32,29 @@ app.use(express.static(`${__dirname}/client/build`));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use((req, res, next) => {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (
-    !req.secure &&
-    req.get("x-forwarded-proto") !== "https" &&
-    process.env.NODE_ENV === "production"
+  if (req.get("X-Forwarded-Proto") == "https" || req.hostname == "localhost") {
+    next();
+  } else if (
+    req.get("X-Forwarded-Proto") != "https" &&
+    req.get("X-Forwarded-Port") != "443"
   ) {
-    return res.redirect("https://" + req.get("host") + req.url);
+    //Redirect if not HTTP with original request URL
+    res.redirect("https://" + req.hostname + req.url);
   }
-  next();
 });
+
+// app.use((req, res, next) => {
+//   // The 'x-forwarded-proto' check is for Heroku
+//   if (
+//     !req.secure &&
+//     req.get("x-forwarded-proto") !== "https" &&
+//     process.env.NODE_ENV === "production"
+//   ) {
+//     return res.redirect("https://" + req.get("host") + req.url);
+//   }
+//   next();
+// });
 
 app.get("/api", (req, res) => {
   res.json({ message: "API root" });
