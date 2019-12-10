5c5c4b233f3191517254947e4e1b901cf685d5f7
Reginald Harris <reginaldharris@Administrators-MacBook-Pro.local>
2019-12-10 12:16:54 -0500

    commits changes

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -11,21 +11,21 @@ const Home = (props) => {
                         <img
                             class="d-block w-100"
                             src="https://images.unsplash.com/photo-1521823557102-529508ac809e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80"
-                            alt="First slide"
+                            alt="Muscle car"
                         />
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
                             class="d-block w-100"
                             src="https://images.unsplash.com/photo-1552519507-848ebd4028d3?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-                            alt="Second slide"
+                            alt="Muscle car"
                         />
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
                             class="d-block w-100"
                             src="https://images.unsplash.com/photo-1522120177514-2b16ebe5634d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-                            alt="Third slide"
+                            alt="Muscle car"
                         />
                     </Carousel.Item>
                 </Carousel>
